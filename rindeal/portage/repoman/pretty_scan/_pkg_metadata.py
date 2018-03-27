7e2f071ba20d214c22edd56a62fa3cb7fe75fe0d
Jan Chren (rindeal) <dev.rindeal@gmail.com>
2018-03-27 06:37:34 +0200

    bump metadata version

--- rindeal/portage/repoman/pretty_scan/_pkg_metadata.py
+++ rindeal/portage/repoman/pretty_scan/_pkg_metadata.py
@@ -1,7 +1,7 @@
 class metadata:
     name = "repoman-pretty-scan"
     short_description = "Pretty Repoman Scan"
-    version = "0.3.0"
+    version = "0.4.0"
     url = f"https://bitbucket.org/rindeal_py/{name}"
     author = "Jan Chren (rindeal)"
     author_email = f"dev+{name}@janchren.eu"
