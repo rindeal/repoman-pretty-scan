ed41aa3572653b9a839414a592da14284c497358
Jan Chren (rindeal) <dev.rindeal@gmail.com>
2018-07-24 19:25:42 +0200

    update

--- setup.py
+++ setup.py
@@ -1,28 +1,31 @@
-#!/usr/bin/env python3.6
+#!/usr/bin/env python3
+# Compatibility: python3.6+
 ##
-# Copyright (C) 2018  Jan Chren (rindeal)
+# Copyright (C) 2018 Jan Chren (rindeal)
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-only
 ##
 
 from distutils.core import setup
+import os
 import sys
 
 
 # make sure local modules are imported
-sys.path.insert(0, ".")
-from rindeal.portage.repoman.pretty_scan._pkg_metadata import metadata
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "lib"))
+from rindeal.portage.repoman.pretty_scan import __PkgMetadata__ as metadata
 
 
 setup(
@@ -30,17 +33,31 @@ setup(
 	name=metadata.name,
 	version=metadata.version,
 	url=metadata.url,
+	description=metadata.summary,
 
 	# >> creator section
 	author=metadata.author,
 	author_email=metadata.author_email,
-	license=metadata.licence_name,
+	license=metadata.licence_identifier,
 
 	# >> dependencies
-	# https://docs.python.org/3/distutils/setupscript.html#relationships-between-distributions-and-packages
-	requires=("rindeal.travis_ci.utils",),
+	platforms=(
+		"linux",
+		"python3.6+",
+	),
+	requires=(
+		"repoman",
+		"rindeal.travis_ci.utils",
+	),
 
 	# >> stuff to actually do
-	packages=("rindeal.portage.repoman.pretty_scan",),
-	scripts=(f"bin/{metadata.name}",),
+	package_dir={
+		'': 'lib',
+	},
+	packages=(
+		"rindeal.portage.repoman.pretty_scan",
+	),
+	scripts=(
+		"bin/repoman-pretty-scan",
+	),
 )
